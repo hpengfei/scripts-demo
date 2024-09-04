9f65382e90a2b258125ffb450de9f86cd774a8e6
ayi <ayi@hicore.com>
2024-09-04 15:49:05 +0800

    add

---
+++ python/base_python.py
@@ -0,0 +1,25 @@
+# sum = 0
+# for i in range(11):
+#     print(i, end=' ')
+#     sum = sum + i
+#
+# print()
+# print(sum)
+#
+
+# 润年
+years = input('请输入年份')
+
+if isinstance(int(years), int):
+    print('{0} is int.'.format(years))
+    # if (int(years) / 4 )== 0:
+    #     print('{0} 是闰年.'.format(years))
+else:
+    print('{0} in not int.'.format(years))
+
+# print(years)
+
+# 100 以内素数
+
+
+
